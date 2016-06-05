f36f420e0ca434581b1f14761b47c77a8154fac3
William Gard <Olegmixa@mail.ru>
2016-06-05 22:37:13 +0300

    Revert "St -1.0"
    This reverts commit c3bb2b496b9c508ece38d07fd41383d4f368a245.

--- Old/db.php
+++
@@ -1,5 +0,0 @@
-<?php
-$db = mysql_connect("localhost","root","") or die("MySQL сервер недоступен!".mysql_error());
-mysql_select_db('diplom', $db) or die("Нет соединения с БД".mysql_error());
-mysql_set_charset("utf8");
-?>
