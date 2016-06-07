480949b627fc7f668d43adc1f01640c415993904
Denis <Den_90rus@mail.ru>
2016-06-07 21:28:30 +0300

    user_st_0.10

---
+++ application/views/global/auth.php
@@ -0,0 +1,10 @@
+			<div id='auth' class="navbar-collapse collapse">
+				<form class="navbar-form navbar-right" name='logform' role="form">
+					<div class="form-group">
+						<a href="#"><label class="btn btn-warning">Личный кабинет</label></a>
+					</div>
+					<div class="form-group">
+						<?php echo $res; ?>
+					</div>
+				</form>
+			</div>
