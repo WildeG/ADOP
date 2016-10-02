783f9732c44a5cc5dba3459c76ce7be5e31c4e15
Denis <Den_90rus@mail.ru>
2016-10-02 21:10:35 +0300

    user 3

--- application/views/home.php
+++ application/views/home.php
@@ -1,63 +1,27 @@
 <div class="container col-xs-9 col-md-offset-1">
 	<h1 class="text-center" style="margin-bottom: 40px;">Панель управления</h1>
-	<div class="form-group col-xs-12">
-		<a href="<?php echo URL::base();?>project">
+		<a class="col-md-4" href="<?php echo URL::base();?>project">
 			<div class="text-center col-xs-4">
 				<img src="<?php echo URL::base(); ?>public/image/system/project.png">
 				<p class="text-center col-xs-12"><h2><small>Проекты</small></h2></p>
 			</div>
 		</a>
-		<a href="<?php echo URL::base();?>users">
-			<div class="text-center col-xs-4">
-				<img src="<?php echo URL::base(); ?>public/image/system/users.png">
-				<p class="text-center col-xs-12"><h2><small>Пользователи</small></h2></p>
-			</div>
-		</a>
-		<a href="<?php echo URL::base();?>specialties">
-			<div class="text-center col-xs-4">
-				<img src="<?php echo URL::base(); ?>public/image/system/specialties.png">
-				<p class="text-center col-xs-12"><h2><small>Специальности</small></h2></p>
-			</div>
-		</a>
-	</div>
-	<div class="form-group col-xs-12">
-		<a href="<?php echo URL::base();?>group">
-			<div class="text-center col-xs-4">
-				<img src="<?php echo URL::base(); ?>public/image/system/group.png">
-				<p class="text-center col-xs-12"><h2><small>Группы</small></h2></p>
-			</div>
-		</a>
-		<a href="<?php echo URL::base();?>topics">
+		<a class="col-md-4" href="<?php echo URL::base();?>topics">
 			<div class="text-center col-xs-4">
 				<img src="<?php echo URL::base(); ?>public/image/system/topics.png">
 				<p class="text-center col-xs-12"><h2><small>Возможные темы</small></h2></p>
 			</div>
 		</a>
-		<a href="<?php echo URL::base();?>manager">
+		<a class="col-md-4" href="<?php echo URL::base();?>manager">
 			<div class="text-center col-xs-4">
 				<img src="<?php echo URL::base(); ?>public/image/system/manager.png">
 				<p class="text-center col-xs-12"><h2><small>Руководители</small></h2></p>
 			</div>
 		</a>
-	</div>
-	<div class="form-group">
-		<a href="<?php echo URL::base();?>view_project">
-			<div class="text-center col-xs-4">
-				<img src="<?php echo URL::base(); ?>public/image/system/view_project.png">
-				<p class="text-center col-xs-12"><h2><small>Виды проекта</small></h2></p>
-			</div>
-		</a>
-		<a href="<?php echo URL::base();?>report">
-			<div class="text-center col-xs-4">
-				<img src="<?php echo URL::base(); ?>public/image/system/report.png">
-				<p class="text-center col-xs-12"><h2><small>Отчеты</small></h2></p>
-			</div>
-		</a>
-		<a href="<?php echo URL::base();?>exit">
+		<a class="col-md-4" href="<?php echo URL::base();?>exit">
 			<div class="text-center col-xs-4">
 				<img src="<?php echo URL::base(); ?>public/image/system/exit.png">
 				<p class="text-center col-xs-12"><h2><small>Выход</small></h2></p>
 			</div>
 		</a>
-	</div>
 </div>
